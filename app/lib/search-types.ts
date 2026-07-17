296c943f53c17a3c7753f4a4ceb6ada0d69891a7
beastmode2000 <nickt@arcticmgnt.com>
2026-07-16 20:17:36 -0700

    Update search-types.ts

--- app/lib/search-types.ts
+++ app/lib/search-types.ts
@@ -1,1 +1,15 @@
+import type { SearchResult } from "./atlas-types";
+
+export type AtlasSearchRecord = SearchResult & {
+  searchableText: string;
+  normalizedTitle: string;
+  normalizedType: string;
+  tokens: string[];
+};
+
+export type AtlasSearchMatch = {
+  item: SearchResult;
+  score: number;
+  matchedTerms: string[];
+};
 
