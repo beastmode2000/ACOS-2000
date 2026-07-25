8d78c8f19cd17014a21cef84d3a857b0221c882f
beastmode2000 <nickt@arcticmgnt.com>
2026-07-25 15:18:23 -0700

    Update AtlasWorkOrders.tsx

--- components/AtlasWorkOrders.tsx
+++ components/AtlasWorkOrders.tsx
@@ -427,8 +427,9 @@ export function AtlasWorkOrders(props: AtlasWorkOrdersProps) {
 
   const searchControlStyle: React.CSSProperties = {
     ...controlStyle,
-    border: `1px solid ${colors.navy}`,
-    boxShadow: "0 0 0 1px rgba(15, 31, 48, 0.05)",
+    border: `2px solid ${colors.navy}`,
+    boxShadow: "0 0 0 3px rgba(15, 31, 48, 0.10)",
+    outline: "none",
   };
 
   const filterPanelStyle: React.CSSProperties = {
