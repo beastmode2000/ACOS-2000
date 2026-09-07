df107aeefe94d6bf0a1e674fc345a71979837559
beastmode2000 <nickt@arcticmgnt.com>
2026-09-07 12:04:16 -0700

    Enable location photo editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./atlas-visual-system.css";
 import AtlasLocationsPolish from "./components/AtlasLocationsPolish";
 import AtlasLocationsViewportPolish from "./components/AtlasLocationsViewportPolish";
 import AtlasLocationDetailPolish from "./components/AtlasLocationDetailPolish";
+import AtlasLocationPhotoEditor from "./components/AtlasLocationPhotoEditor";
 import AtlasPropertyVisibility from "./components/AtlasPropertyVisibility";
 import AtlasWorkspacePolish from "./components/AtlasWorkspacePolish";
 import AtlasAssetReferencePolish from "./components/AtlasAssetReferencePolish";
@@ -57,6 +58,7 @@ export default function RootLayout({
         <AtlasLocationsPolish />
         <AtlasLocationsViewportPolish />
         <AtlasLocationDetailPolish />
+        <AtlasLocationPhotoEditor />
         <AtlasPropertyVisibility />
         <AtlasWorkspacePolish />
         <AtlasAssetReferencePolish />
