468651f5fc8da0a011e14b85ae708ee1fb8d825d
beastmode2000 <nickt@arcticmgnt.com>
2026-09-11 19:54:21 -0700

    Apply clean Work list styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ import AtlasAssetEditVisibilityFix from "./components/AtlasAssetEditVisibilityFi
 import AtlasAssetsViewportPolish from "./components/AtlasAssetsViewportPolish";
 import AtlasDepartmentAssetPhotoPolish from "./components/AtlasDepartmentAssetPhotoPolish";
 import AtlasWorkPolish from "./components/AtlasWorkPolish";
+import AtlasWorkListPolish from "./components/AtlasWorkListPolish";
 import AtlasWorkCategoryCanonicalizer from "./components/AtlasWorkCategoryCanonicalizer";
 import AtlasWorkEditorCleanup from "./components/AtlasWorkEditorCleanup";
 import AtlasWorkWeekWrap from "./components/AtlasWorkWeekWrap";
@@ -112,6 +113,7 @@ export default function RootLayout({
         <AtlasAssetsViewportPolish />
         <AtlasDepartmentAssetPhotoPolish />
         <AtlasWorkPolish />
+        <AtlasWorkListPolish />
         <AtlasWorkCategoryCanonicalizer />
         <AtlasWorkEditorCleanup />
         <AtlasWorkWeekWrap />
