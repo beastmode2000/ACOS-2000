411e6f91d577c3e1d98cea1315f7a361cd376e3c
beastmode2000 <nickt@arcticmgnt.com>
2026-09-11 18:44:33 -0700

    Restore last known-good layout before owner report fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,6 @@ import AtlasHouseMaintenancePolish from "./components/AtlasHouseMaintenancePolis
 import AtlasNavigationSafety from "./components/AtlasNavigationSafety";
 import AtlasOperationsPolish from "./components/AtlasOperationsPolish";
 import AtlasOwnerReportHeaderPolish from "./components/AtlasOwnerReportHeaderPolish";
-import AtlasOwnerReportRoutineClassificationFix from "./components/AtlasOwnerReportRoutineClassificationFix";
 import AtlasServiceLinkPolish from "./components/AtlasServiceLinkPolish";
 import AtlasWorkHistorySearch from "./components/AtlasWorkHistorySearch";
 import AtlasSidebarScrollbarPolish from "./components/AtlasSidebarScrollbarPolish";
@@ -136,7 +135,6 @@ export default function RootLayout({
         <AtlasNavigationSafety />
         <AtlasOperationsPolish />
         <AtlasOwnerReportHeaderPolish />
-        <AtlasOwnerReportRoutineClassificationFix />
         <AtlasServiceLinkPolish />
         <AtlasWorkHistorySearch />
         <AtlasScrollSafety />
