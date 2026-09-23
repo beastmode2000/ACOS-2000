544eb7eaee88d313a41432548f350c792d674bc9
beastmode2000 <nickt@arcticmgnt.com>
2026-09-23 09:18:29 -0700

    Atlas: recover automatically after idle tabs resume

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AtlasApp from "./components/AtlasApp";
 import AtlasBootGate from "./components/AtlasBootGate";
+import AtlasResumeRecovery from "./components/AtlasResumeRecovery";
 
 export default function Page() {
   return (
     <AtlasBootGate>
+      <AtlasResumeRecovery />
       <AtlasApp />
     </AtlasBootGate>
   );
