49131e1aa2308f73f235d531b15a83f490730e77
beastmode2000 <nickt@arcticmgnt.com>
2026-09-23 10:20:04 -0700

    Load Saturday Event Prep setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import AtlasApp from "./components/AtlasApp";
 import AtlasBootGate from "./components/AtlasBootGate";
 import AtlasResumeRecovery from "./components/AtlasResumeRecovery";
+import AtlasSaturdayEventPrepSetup from "./components/AtlasSaturdayEventPrepSetup";
 
 export default function Page() {
   return (
     <AtlasBootGate>
       <AtlasResumeRecovery />
+      <AtlasSaturdayEventPrepSetup />
       <AtlasApp />
     </AtlasBootGate>
   );
